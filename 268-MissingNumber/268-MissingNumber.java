901a149c2188310613fd7c6fca5732466438b58f
24149015-tech <24149015@drngpit.ac.in>
2026-03-10 14:52:26 +0530

    Create 268-MissingNumber

---
+++ 268-MissingNumber/268-MissingNumber.java
@@ -0,0 +1,13 @@
+// Last updated: 3/10/2026, 2:52:26 PM
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int sum=n*(n+1)/2;
+        int news=0;
+        for(int i=0;i<nums.length;i++){
+             news+=nums[i];
+        } 
+        return sum-news;    
+    }
+
+}
