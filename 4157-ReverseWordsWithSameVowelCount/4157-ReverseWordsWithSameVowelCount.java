4873a32509c92c51b67cd19bffc35e6e818dfe9b
24149015-tech <24149015@drngpit.ac.in>
2026-03-10 14:51:39 +0530

    Create 4157-ReverseWordsWithSameVowelCount

---
+++ 4157-ReverseWordsWithSameVowelCount/4157-ReverseWordsWithSameVowelCount.java
@@ -0,0 +1,22 @@
+// Last updated: 3/10/2026, 2:51:39 PM
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        int targetVowels = countVowels(words[0]);
+         for (int i = 1; i < words.length; i++) {
+            if (countVowels(words[i]) == targetVowels) {
+                words[i] = new StringBuilder(words[i]).reverse().toString();
+            }
+        }
+        return String.join(" ", words);
+    }
+    private int countVowels(String word) {
+        int count = 0;
+        for (char c : word.toCharArray()) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
