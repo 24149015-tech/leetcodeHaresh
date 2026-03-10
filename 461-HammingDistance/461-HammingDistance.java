7c05d4a646a6b56862d1c1a6591b97fa7cc12fdc
24149015-tech <24149015@drngpit.ac.in>
2026-03-10 14:52:20 +0530

    Create 461-HammingDistance

---
+++ 461-HammingDistance/461-HammingDistance.java
@@ -0,0 +1,16 @@
+// Last updated: 3/10/2026, 2:52:20 PM
+class Solution {
+    public int hammingDistance(int x, int y) {
+         int xorResult = x ^ y;
+        int count = 0;
+        
+        while (xorResult != 0) {
+            count += xorResult & 1;
+            xorResult >>= 1;
+        }
+        
+        return count;
+
+        
+    }
+}
