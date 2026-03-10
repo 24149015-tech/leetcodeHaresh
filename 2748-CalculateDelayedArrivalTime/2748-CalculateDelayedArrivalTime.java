54aae0e80f51abfc7b3d5bdefbb6154ee7cc8379
24149015-tech <24149015@drngpit.ac.in>
2026-03-10 14:51:47 +0530

    Create 2748-CalculateDelayedArrivalTime

---
+++ 2748-CalculateDelayedArrivalTime/2748-CalculateDelayedArrivalTime.java
@@ -0,0 +1,10 @@
+// Last updated: 3/10/2026, 2:51:46 PM
+class Solution {
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        int palani = arrivalTime + delayedTime;
+        return palani%24;
+
+     
+
+    }
+}
