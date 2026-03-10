feb2d2a105bf9df6a4931a4091e32cc02224248b
24149015-tech <24149015@drngpit.ac.in>
2026-03-10 14:52:25 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,8 @@
+// Last updated: 3/10/2026, 2:52:25 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4!=0) return true ;
+        else return false;
+        }
+        
+    }
